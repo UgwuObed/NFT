f9c9df0fd9c2a9e4723ba36de141f8a6c36c3e49
Ugwu Obed <vjazzyvictor@gmail.com>
2022-07-14 20:00:19 +0100

    adding fix again

--- NFT-Tutorial/NFTee.sol
+++ NFT-Tutorial/NFTee.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 // GameItem is  ERC721 signifies that the contract we are creating imports ERC721 and follows ERC721 contract from openzeppelin
 contract GameItem is ERC721 {
-    constructor() ERC721("MoonPaws", "MPW") {
+    constructor() ERC721("GameItem", "ITM") {
         // mint an NFT to yourself
         _mint(msg.sender, 1);
         _mint(msg.sender, 2);
--- NFT-Tutorial/hardhat.config.js
+++ NFT-Tutorial/hardhat.config.js
@@ -11,8 +11,11 @@ module.exports = {
   solidity: "0.8.0",
   networks: {
     rinkeby: {
-      url: process.env.ALCHEMY_API_KEY_URL,
-      accounts: [process.env.RINKEBY_PRIVATE_KEY],
+      url: "https://eth-rinkeby.alchemyapi.io/v2/WlBvP8wMbrd8FPw-AWwka2Izn0WXocSs",
+      accounts: [
+        "8305f28a50f2bf161c9a0b1a4ecc991e7c235e0ff6979ff85933fc2d6724a836",
+    
+    ],
     },
   },
 };
